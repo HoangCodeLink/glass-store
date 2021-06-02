dfb732eb2f9b187b269fe9a08ae2d7d760acfbbb
Hoang Vo <hoang.vo@codelink.io>
2021-06-02 16:39:32 +0700

    Create nav bar layout

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,31 @@
+import { ThemeProvider } from '@material-ui/styles';
+import NavBar from '../NavBar';
 import './App.css';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+
+const theme = responsiveFontSizes(
+	createMuiTheme({
+		palette: {
+			type: 'dark',
+			primary: {
+				main: '#2b3030',
+			},
+			secondary: {
+				main: '#ffffff',
+			}
+		},
+	})
+);
 
 function App() {
-  return (
-    <div className="App">
-      Hello, World
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<div className='App'>
+				<NavBar />
+				<h2>Product List</h2>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,73 @@
+import {
+	AppBar,
+	Badge,
+	Button,
+	IconButton,
+	makeStyles,
+	Toolbar,
+	Typography,
+} from '@material-ui/core';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+import React from 'react';
+import { Add } from '@material-ui/icons';
+
+const NavBar = (props) => {
+	const classes = useStyles();
+
+	return (
+		<AppBar position='static'>
+			<Toolbar>
+				<IconButton
+					edge='start'
+					className={classes.menuButton}
+					color='inherit'
+					aria-label='logo'>
+					<img className={classes.img} alt='complex' src='/logo.png' />
+				</IconButton>
+				<Typography className={classes.title} variant='h6' noWrap>
+					Eyeglass Store
+				</Typography>
+				<div className={classes.grow} />
+				<Button
+					edge='start'
+					color='inherit'
+					variant='outlined'
+					aria-label='New Product'
+					className={classes.button}
+					aria-controls='NewProduct'
+					title='New Product'
+					size='medium'
+					startIcon={<Add />}>
+					<span className={classes.buttonText}>New Product</span>
+				</Button>
+				<IconButton aria-label='show 4 new mails' color='inherit'>
+					<Badge badgeContent={4} color='secondary'>
+						<ShoppingCartIcon />
+					</Badge>
+				</IconButton>
+			</Toolbar>
+		</AppBar>
+	);
+};
+
+const useStyles = makeStyles((theme) => ({
+	grow: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		display: 'none',
+		[theme.breakpoints.up('sm')]: {
+			display: 'block',
+		},
+	},
+	img: {
+		width: 90,
+		height: 90,
+	},
+}));
+
+export default NavBar;
